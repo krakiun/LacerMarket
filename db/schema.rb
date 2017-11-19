d83a37fd5d04415edbeca679771ccc26358e598d
krakiun <krakiun@gmail.com>
2017-11-19 12:01:02 +0200

    add contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119002231) do
+ActiveRecord::Schema.define(version: 20171119095725) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string   "titolo"
+    t.text     "descrizione"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
